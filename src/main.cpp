70d4510b2b656abc0736382851420372be30f6ae
PlamEmil <e2039447@cmaisonneuve.qc.ca>
2024-11-22 18:00:00 -0500

    enlever info inutile

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,6 @@
 #include <ArduinoJson.h>
 #include "connectWiFi.h"
 
-const char *SSID = "Plamondon";
-const char *PASSWORD = "Plamondon123";
-
-#define API_KEY "db51c8ff-50bd-48d7-8a48-765cb4bfead9"
-#define API_HOST "api.simplyprint.io/12305/endpoint/"
-
 MenuJoystick menu(A0, A3, 26, 27);
 
 void setup()
