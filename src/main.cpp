2c0c742688a0aad572ca9e82808a34d7cddceb56
PlamEmil <e2039447@cmaisonneuve.qc.ca>
2024-11-23 17:34:44 -0500

    ajout lenvoi de printerState quand requete

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,14 @@
 
 #define SLAVE_ADDRESS 0x50
 
+const char *printerState;
+
 void setup()
 {
   Wire.begin();
+  Wire.onRequest(onRequestHandler);
+
+
   Serial.begin(9600);
 
   connectWiFi();
@@ -17,5 +22,29 @@ void setup()
 
 void loop()
 {
-  
+  if (millis() % 5000 == 0)
+  {
+    printerState = getPrinters();
+    if (strcmp(printerState, "printing") == 0)
+    {
+      Serial.println("Printing");
+    }
+    else if (strcmp(printerState, "paused") == 0)
+    {
+      Serial.println("Paused");
+    }
+    else if (strcmp(printerState, "idle") == 0)
+    {
+      Serial.println("Idle");
+    }
+    else if (strcmp(printerState, "operational") == 0)
+    {
+      Serial.println("Operational");
+    }
+  }
+}
+
+void onRequestHandler()
+{
+  Wire.write(printerState);
 }
