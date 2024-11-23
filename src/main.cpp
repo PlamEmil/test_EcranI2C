b906440dde9f1f28ae0b24c6472628344d41e07d
PlamEmil <e2039447@cmaisonneuve.qc.ca>
2024-11-23 17:25:46 -0500

    include les libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+#include "arduino_secrets.h"
+#include "api.h"
+#include "connectWiFi.h"
+
 #define SLAVE_ADDRESS 0x50
 
 void setup()
 {
   Wire.begin();
   Serial.begin(9600);
+
+  connectWiFi();
 }
 
 void loop()
