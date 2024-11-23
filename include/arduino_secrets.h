d7c08b50f7c0765f08ba9e8a89f180296b32fc69
PlamEmil <e2039447@cmaisonneuve.qc.ca>
2024-11-23 17:22:48 -0500

    fichier dinformations de connexion

---
+++ include/arduino_secrets.h
@@ -0,0 +1,8 @@
+#define WIFI_ENTERPRISE
+#define EAP_IDENTITY "cvwo@eva.eduroam.ca"
+#define EAP_USERNAME "cvwo@eva.eduroam.ca"
+#define EAP_PASSWORD "pyknm"
+#define SECRET_SSID "eduroam"
+
+#define API_KEY "db51c8ff-50bd-48d7-8a48-765cb4bfead9"
+#define API_HOST "api.simplyprint.io/12305/endpoint/"
