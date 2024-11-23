a91cfa68cf3e4974ea4d031a63e5718799ad79f0
PlamEmil <e2039447@cmaisonneuve.qc.ca>
2024-11-23 17:25:33 -0500

    changer connexion au wifi

--- src/connectWiFi.cpp
+++ src/connectWiFi.cpp
@@ -7,14 +7,24 @@
 
 void connectWiFi()
 {
+
+// Connexion au réseau wifi sécurisé avec identifiant et mot de passe
+#ifdef WIFI_ENTERPRISE
     // Connexion au réseau wifi sécurisé avec identifiant et mot de passe
-    //WiFi.begin(SECRET_SSID, WPA2_AUTH_PEAP, EAP_IDENTITY, EAP_USERNAME, EAP_PASSWORD);
+    WiFi.begin(SECRET_SSID, WPA2_AUTH_PEAP, EAP_IDENTITY, EAP_USERNAME, EAP_PASSWORD);
+#else
+    // Connexion au réseau wifi standard
     WiFi.begin("Plamondon", "Plamondon123");
+#endif
     // Attendre la connexion au réseau wifi
     while (WiFi.status() != WL_CONNECTED)
     {
         Serial.print("Attempting to connect to Network named: ");
+#ifdef WIFI_ENTERPRISE
         Serial.println(SECRET_SSID); // print the network name (SSID)
+#else
+        Serial.println("Plamondon"); // print the network name (SSID)
+#endif
         delay(2000);
     }
 
